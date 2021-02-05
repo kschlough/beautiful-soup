054ac438b5cb5b1e3af1e75d5f8fa72c750cfd94
kschlough <61514488+kschlough@users.noreply.github.com>
2021-02-04 18:04:16 -0800

    Working code with https://www.tmcfinancing.com/covid-19-grants/ as the scraped url

--- webscraper.py
+++ webscraper.py
@@ -1,14 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://www.pyladies.com"
-r = requests.get(url)
+# this code works!
+url = "https://www.tmcfinancing.com/covid-19-grants/"
+r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
 print(r.status_code)
 
 soup = BeautifulSoup(r.text, "html.parser")
-soup.find_all('a')
+soup.find_all('href')
 print(soup.prettify())
+
+
+# testing next url
+
+
+
 
 
 
