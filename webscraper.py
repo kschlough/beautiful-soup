12fe22671f7eb160f9f23bb5bb6562cdf854ea14
kschlough <61514488+kschlough@users.noreply.github.com>
2021-02-04 18:18:59 -0800

    Saved link titles using get_text() method + links in a dictionary

--- webscraper.py
+++ webscraper.py
@@ -1,26 +1,44 @@
-import requests
-from bs4 import BeautifulSoup
-
-# this code works!
-url = "https://www.tmcfinancing.com/covid-19-grants/"
-r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-
-print(r.status_code)
-
-soup = BeautifulSoup(r.text, "html.parser")
-soup.find_all('href')
-print(soup.prettify())
-
-
-# testing next url
-
-
-
-
-
+### notes
 
 # if 403 error: pretend i'm firefox's open-source renderer
 # r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
 # find single (find) vs all
 # soup.find_all(x)
+
+
+
+
+import requests
+from bs4 import BeautifulSoup
+
+### this code works!
+url = "https://www.tmcfinancing.com/covid-19-grants/"
+r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+print(r.status_code)
+
+soup = BeautifulSoup(r.text, "html.parser")
+### commented out for now while doing other tests
+# soup.find_all('a')[2].attrs['href']
+# print(soup.prettify())
+
+### test: extract links from website into data structure
+### maybe store in dictionary not array?
+links = {}
+
+### using get_text() method
+for link in soup.find_all('a'):  
+    links[link.get_text()] = link
+
+print(links)
+
+### 
+
+
+
+
+
+
+
+
